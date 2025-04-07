75e1fd58da26910e8812eca9916a079ae64d7c5c
korneliuszburian <krnijuu@gmail.com>
2025-04-07 11:27:45 +0200

    refactor: ♻️ added new format selector

--- config/services.php
+++ config/services.php
@@ -5,113 +5,114 @@
  */
 
 $container->set(
-	'plugin',
-	function ( $c ) {
-		return new \WpImageOptimizer\Core\Plugin( $c );
-	}
+  'plugin',
+  function ($c) {
+    return new \WpImageOptimizer\Core\Plugin($c);
+  }
 );
 
 $container->set(
-	'settings',
-	function ( $c ) {
-		return new \WpImageOptimizer\Core\Settings();
-	}
+  'settings',
+  function ($c) {
+    return new \WpImageOptimizer\Core\Settings();
+  }
 );
 
 $container->set(
-	'logger',
-	function ( $c ) {
-		return new \WpImageOptimizer\Utility\Logger();
-	}
+  'logger',
+  function ($c) {
+    return new \WpImageOptimizer\Utility\Logger();
+  }
 );
 
 $container->set(
-	'progress_manager',
-	function ( $c ) {
-		return new \WpImageOptimizer\Utility\ProgressManager();
-	}
+  'progress_manager',
+  function ($c) {
+    return new \WpImageOptimizer\Utility\ProgressManager();
+  }
 );
 
 $container->set(
-	'stats',
-	function ( $c ) {
-		return new \WpImageOptimizer\Utility\Stats();
-	}
+  'stats',
+  function ($c) {
+    return new \WpImageOptimizer\Utility\Stats();
+  }
 );
 
 $container->set(
-	'webp_converter',
-	function ( $c ) {
-		return new \WpImageOptimizer\Conversion\WebpConverter(
-			$c->get( 'settings' ),
-			$c->get( 'logger' )
-		);
-	}
+  'webp_converter',
+  function ($c) {
+    return new \WpImageOptimizer\Conversion\WebpConverter(
+      $c->get('settings'),
+      $c->get('logger')
+    );
+  }
 );
 
 $container->set(
-	'avif_converter',
-	function ( $c ) {
-		return new \WpImageOptimizer\Conversion\AvifConverter(
-			$c->get( 'settings' ),
-			$c->get( 'logger' )
-		);
-	}
+  'avif_converter',
+  function ($c) {
+    return new \WpImageOptimizer\Conversion\AvifConverter(
+      $c->get('settings'),
+      $c->get('logger')
+    );
+  }
+);
+
+$container->set(
+  'format_selector',
+  function ($c) {
+    return new \WpImageOptimizer\Media\FormatSelector(
+      $c->get('settings'),
+      $c->get('logger')
+    );
+  }
 );
 
 // Media services
 $container->set(
-	'media_processor',
-	function ( $c ) {
-		return new \WpImageOptimizer\Media\MediaProcessor(
-			$c->get( 'webp_converter' ),
-			$c->get( 'avif_converter' ),
-			$c->get( 'settings' ),
-			$c->get( 'progress_manager' ),
-			$c->get( 'logger' )
-		);
-	}
+  'media_processor',
+  function ($c) {
+    return new \WpImageOptimizer\Media\MediaProcessor(
+      $c->get('webp_converter'),
+      $c->get('avif_converter'),
+      $c->get('settings'),
+      $c->get('progress_manager'),
+      $c->get('logger'),
+      $c->get('format_selector')
+    );
+  }
 );
 
 $container->set(
-	'media_library_integration',
-	function ( $c ) {
-		return new \WpImageOptimizer\Media\MediaLibraryIntegration(
-			$c->get( 'settings' ),
-			$c->get( 'media_processor' ),
-			$c->get( 'logger' )
-		);
-	}
+  'media_library_integration',
+  function ($c) {
+    return new \WpImageOptimizer\Media\MediaLibraryIntegration(
+      $c->get('settings'),
+      $c->get('media_processor'),
+      $c->get('logger')
+    );
+  }
 );
 
 $container->set(
-    'admin_page',
-    function ( $c ) {
-        return new \WpImageOptimizer\Admin\AdminPage(
-            $c, // pass the container here
-            $c->get( 'settings' ),
-            $c->get( 'stats' ),
-            $c->get( 'logger' ),
-            $c->get( 'progress_manager' )
-        );
-    }
+  'admin_page',
+  function ($c) {
+    return new \WpImageOptimizer\Admin\AdminPage(
+      $c, // pass the container here
+      $c->get('settings'),
+      $c->get('stats'),
+      $c->get('logger'),
+      $c->get('progress_manager')
+    );
+  }
 );
 
 $container->set(
-	'dashboard_widget',
-	function ( $c ) {
-		return new \WpImageOptimizer\Admin\DashboardWidget(
-			$c->get( 'stats' )
-		);
-	}
-);
-
-// Frontend services
-$container->set(
-	'image_delivery',
-	function ( $c ) {
-		return new \WpImageOptimizer\Frontend\ImageDelivery(
-			$c->get( 'settings' )
-		);
-	}
+  'dashboard_widget',
+  function ($c) {
+    return new \WpImageOptimizer\Admin\DashboardWidget(
+      $c->get('stats')
+    );
+  }
 );
